01fa87a2cdbd9d0effc9b0a4fc57c9ab429a29bc
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-02 21:36:58 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   new_array = []
-  array.each do { |new_array| numbers << new_array ** 2 }
-    numbers
+  array.each do { |numbers| new_array << numbers ** 2 }
+    new_array
 end
 end
